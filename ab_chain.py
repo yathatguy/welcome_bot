fcf020bdb67c11061fb941a87e88309fcfebb799
Dmitriy Spasskiy <thatguy@yandex-team.ru>
2017-11-28 20:22:29 +0300

    Debugging

--- ab_chain.py
+++ ab_chain.py
@@ -32,19 +32,27 @@ def on_user_joins(bot, update):
     global MESSAGE_ID
     query = get_query(bot, update)
     if len(query.message.new_chat_members) > 0 and query.message.chat.type in ["group", "supergroup"]:
-        logging.info(query.message)
-        if query.message.new_chat_member.username != None:
-            text = WELCOME_TEXT.format(u'@' + query.message.new_chat_member.username)
-        else:
-            text = WELCOME_TEXT.format('stranger')
-        bot.sendMessage(text=text, chat_id=query.message.chat.id)
-        logging.info("Message {}, last info package was sent on greeting {}".format(query.message.message_id, MESSAGE_ID))
-        if query.message.message_id > MESSAGE_ID + STEP:
-            filedata = open("greeting.txt", "r")
-            info_package = filedata.read()
-            filedata.close()
-            bot.sendMessage(text=info_package, chat_id=query.message.chat.id, disable_web_page_preview=False)
-            MESSAGE_ID = query.message.message_id
+        for user in query.message.new_chat_members:
+            if user.username != None:
+                text = WELCOME_TEXT.format(u'@' + user.username)
+            else:
+                name = None
+                if "first_name" in user.keys():
+                    name = name + user.first_name
+                if "last_name" in user.keys():
+                    name = name + ' ' + user.last_name
+                if name:
+                    text = WELCOME_TEXT.format(name)
+                else:
+                    text = WELCOME_TEXT.format('stranger')
+            bot.sendMessage(text=text, chat_id=query.message.chat.id)
+            logging.info("Message {}, last info package was sent on greeting {}".format(query.message.message_id, MESSAGE_ID))
+            if query.message.message_id > MESSAGE_ID + STEP:
+                filedata = open("greeting.txt", "r")
+                info_package = filedata.read()
+                filedata.close()
+                bot.sendMessage(text=info_package, chat_id=query.message.chat.id, disable_web_page_preview=False)
+                MESSAGE_ID = query.message.message_id
 
 
 def main():
